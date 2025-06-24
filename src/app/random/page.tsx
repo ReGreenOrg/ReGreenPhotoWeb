bc0e2686e2d3f144a20a0dccbf253cf41ac91c39
kimtaeyoon2003122 <k0278kim@gmail.com>
2025-06-24 11:52:03 +0900

    feat: add version change animation

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Aurora from "@/ui/Aurora";
 import Ballpit_t from "@/ui/Ballpit_t";
 import GradientText from "@/ui/GradientText";
@@ -124,20 +124,6 @@ const RandomPage = () => {
           className={"z-10 h-[100vh] w-[100vw]"}
         />
       </motion.div>
-      {isAnimating || resultText !== "" ? (
-        <></>
-      ) : (
-        <div className={"absolute z-30 top-10 right-10 font-bold"}>
-          <GameButton
-            onClick={() => {
-              setVersion(version > 3 ? 1 : version + 1);
-            }}
-            className={"px-10 h-20 rounded-full bg-black opacity-20 text-white text-3xl"}
-          >
-            {version} · {VERSION_TEXT[version - 1]}
-          </GameButton>
-        </div>
-      )}
       {
         isAnimating || resultText !== "" ? <></>:
           <motion.div className={`absolute z-30 font-bold duration-300 ${versionExpand ? "top-20 right-20" : "top-10 right-10"}`}
@@ -185,87 +171,6 @@ const RandomPage = () => {
             </motion.h1>
           </motion.div>
         ) : null}
-        {isAnimating ? (
-          <motion.div
-            className={
-              "absolute bottom-10 px-10 py-5 rounded-full bg-gray-900 border-2 border-gray-800 text-white font-bold text-3xl"
-            }
-            initial={{
-              y: 30,
-              opacity: 0,
-            }}
-            animate={{
-              y: 0,
-              opacity: 1,
-            }}
-          >
-            <GradientText
-              colors={["#ed5a8e", "#ffffff", "#bd63ed"]}
-              animationSpeed={5}
-              showBorder={false}
-              className="custom-class"
-            >
-              <div className={"text-3xl font-black"}>두근두근, 무엇이 나올까요?</div>
-            </GradientText>
-          </motion.div>
-        ) : resultText == "" ? (
-          <CircularGameButton
-            className="w-64 h-64 rounded-full font-extrabold text-5xl border-gray-300 bg-gray-100 hover:bg-gray-100 transition"
-            onClick={() => handleDraw(version)}
-          >
-            뽑기
-          </CircularGameButton>
-        ) : (
-          <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} className="">
-            <GameButton
-              onClick={() => {
-                location.reload();
-              }}
-              className={
-                "backdrop-contrast-200 backdrop-saturate-150 min-w-96 px-10 py-10 font-bold text-3xl text-white border-2 border-white/20 bg-white/10 rounded-full"
-              }
-            >
-              운영진에게 화면을 보여주세요
-            </GameButton>
-          </motion.div>
-        )}
-        className={`absolute top-0 w-[100vw] h-[100vh] z-20 flex flex-col items-center justify-center space-y-10 duration-200 ${resultText ? "backdrop-blur-3xl" : ""}`}
-      > {resultText ? (
-        <motion.div
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          className="text-6xl font-black text-gray-50 flex items-center flex-col space-y-10"
-        >
-          <div className="relative w-64 h-64 flex flex-col items-center">
-            <Image
-              src={imageSrc}
-              alt="추첨 이미지"
-              fill
-              className="object-contain rounded-[32px] z-0"
-            />
-          </div>
-          <GradientText
-            colors={["#ed5a8e", "#ffffff", "#bd63ed"]}
-            animationSpeed={5}
-            showBorder={false}
-            className="custom-class"
-          >
-            <div className={"text-7xl font-black"}>{resultText.split("/*/")[0]}</div>
-          </GradientText>
-          <motion.h1 className={"text-4xl font-medium opacity-80 mt-5"}>
-            {resultText.split("/*/")[1]}
-          </motion.h1>
-        </motion.div>
-      ) : null}
-      {/*<div className="relative w-64 h-64 flex flex-col items-center">*/}
-      {/*  <Image*/}
-      {/*    src={imageSrc}*/}
-      {/*    alt="추첨 이미지"*/}
-      {/*    fill*/}
-      {/*    className="object-contain rounded-[32px] z-0"*/}
-      {/*  />*/}
-      {/*</div>*/}
-
         {
           isAnimating
             ? <motion.div
@@ -312,25 +217,6 @@ const RandomPage = () => {
                   </GameButton>
                 </motion.div>
         }
-
-        {/*<button*/}
-        {/*  className="py-3 rounded-lg border border-green-400 bg-green-50 hover:bg-green-100 transition"*/}
-        {/*  onClick={() => handleDraw(1.2)}*/}
-        {/*>*/}
-        {/*  📸 인스타 완료 (확률 UP)*/}
-        {/*</button>*/}
-        {/*<button*/}
-        {/*  className="py-3 rounded-lg border border-purple-400 bg-purple-50 hover:bg-purple-100 transition"*/}
-        {/*  onClick={() => handleDraw(1.5)}*/}
-        {/*>*/}
-        {/*  📊 인스타, 설문 완료 (확률 UP UP)*/}
-        {/*</button>*/}
-        {/*<button*/}
-        {/*  className="py-3 rounded-lg border border-red-400 bg-red-50 hover:bg-red-100 transition"*/}
-        {/*  onClick={() => handleDraw(2)}*/}
-        {/*>*/}
-        {/*  🔥 인스타, 설문, 가입 완료 (확률 UP UP UP)*/}
-        {/*</button>*/}
       </motion.div>
     </div>
   );
