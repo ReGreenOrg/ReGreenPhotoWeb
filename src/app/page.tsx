c4339a2c9b0064b9e84eecf7e8df953cd40e27fa
kimtaeyoon2003122 <k0278kim@gmail.com>
2025-06-28 00:50:04 +0900

    chore: change title from 우이미 촬영하기 to 우이미 네컷사진, change a description from 8초 촬영 to 10초 촬영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-white to-pink-100 px-4 py-8">
       <div className="bg-white/80 backdrop-blur-md rounded-3xl shadow-2xl p-16 flex flex-col items-center space-y-8 max-w-xl w-full border border-pink-200">
         <h1 className="text-5xl font-extrabold text-pink-500 text-center tracking-tight leading-tight drop-shadow-md">
-          우이미 촬영하기!
+          우이미 네컷사진
         </h1>
         <p className="text-center text-xl text-gray-800 leading-relaxed">
           <span className="font-semibold text-pink-600">퀴즈 푸느라 고생하셨어요!</span> <br />
@@ -19,7 +19,7 @@ const Page = () => {
           우이미와 함께한 순간을 <span className="font-semibold text-pink-500">네컷</span>으로
           남겨보세요!
           <br />
-          <span className="font-medium">8초마다 한 장씩 자동 촬영 🎞️</span> <br />
+          <span className="font-medium">10초마다 한 장씩 자동 촬영 🎞️</span> <br />
           또는, <span className="underline decoration-pink-400 decoration-2">직접 촬영</span>도
           가능해요!
         </p>
